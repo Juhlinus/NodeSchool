90cfb3d8d30aba9ccab25a9c1af99138900d5792
Linus Juhlin <juhlinus@gmail.com>
2015-09-22 16:25:07 +0200

    HTTP FILE SERVER COMPLETE

--- program.js
+++ program.js
@@ -1,20 +1,15 @@
-var net = require('net');
+var http = require('http');
+var fs = require('fs');
 
-var server = net.createServer(function(socket) {
+var server = http.createServer(function(request, response) {
 
-	var date = new Date();
+	var readStream = fs.createReadStream(process.argv[3]);
 
-	var data = date.getFullYear() + '-' + 
-		(date.getMonth() < 10 ? 
-		'0' + (date.getMonth() + 1) : 
-		(date.getMonth() + 1)) + '-' + date.getDate() +
-		' ' + date.getHours() + ':' + 
-		(date.getMinutes() < 10 ? '0' + date.getMinutes() :
-		date.getMinutes());
-
-	socket.write(data + '\n');
-
-	socket.end();
+	// When the entire strea is read
+	readStream.on('open', function() {
+		// Feed (Pipe) stream to response
+		readStream.pipe(response);
+	});
 });
 
 server.listen(process.argv[2]);
