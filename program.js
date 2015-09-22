b86f5ae69b9c1e8842012e77b35410576868d284
Linus Juhlin <juhlinus@gmail.com>
2015-09-22 17:35:12 +0200

    HTTP JSON API SERVER COMPLETE

--- program.js
+++ program.js
@@ -1,19 +1,31 @@
 var http = require('http');
-var fs = require('fs');
+var url = require('url');
 
 var server = http.createServer(function(request, response) {
 
-	request.on('data', function(data) {
-		if (request.method === "POST")
-			response.write(data.toString().toUpperCase());
-	});
-	
-	// When the entire request has been handled
-	// end the response
-	request.on('end', function() {
-		response.end();
-	});
-	
+	// Check method
+	if (request.method == 'GET') 
+	{
+		response.writeHead('200', { 'Content-Type': 
+							'application/json' });
+
+		var parsedRequest = url.parse(request.url, true);
+		var date = new Date(parsedRequest.query['iso']);
+
+		if (parsedRequest.pathname == '/api/parsetime')
+		{
+			date = '{"hour":' + date.getHours() +
+					',"minute":' + date.getMinutes() +
+					',"second":' + date.getSeconds() + '}';
+		}
+		else if (parsedRequest.pathname == '/api/unixtime') 
+		{
+			date = '{"unixtime":' + 
+					Date.parse(date.toISOString()) + '}';
+		}
+
+		response.end(date);
+	}
 });
 
 server.listen(process.argv[2]);
