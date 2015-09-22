74bfd0709ebb8a32c9e9cc39cccd0a049c798e36
Linus Juhlin <juhlinus@gmail.com>
2015-09-22 15:24:11 +0200

    HTTP COLLECT COMPLETE

--- program.js
+++ program.js
@@ -1,8 +1,23 @@
 var http = require('http');
+var bl = require('bl');
 
 http.get(process.argv[2], function(response) {
 	
+	var length = 0;
+	var output;
+
+	response.pipe(bl(
+		function(err, data) {
+			output = data.toString();
+		})
+	);
+
 	response.on('data', function(data) {
-		console.log(data.toString());
+		length += data.toString().length;
+	});
+
+	response.on('end', function(end) {
+		console.log(length);
+		console.log(output);
 	});
 });
